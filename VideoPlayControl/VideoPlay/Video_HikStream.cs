b41cc1050bbb3016f955e666ff0160e3f0a2273b
洪栋城 <thank_you1996@outlook.com>
2018-04-26 17:25:09 +0800

    添加海康流媒体类型

---
+++ VideoPlayControl/VideoPlay/Video_HikStream.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PublicClassCurrency;
+
+namespace VideoPlayControl.VideoPlay
+{
+    public class Video_HikStream : IVideoPlay
+    {
+        public VideoInfo CurrentVideoInfo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public CameraInfo CurrentCameraInfo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public VideoPlaySetting CurrentVideoPlaySet { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IntPtr intptrPlayMain { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Enum_VideoPlayState VideoPlayState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event VideoPlayEventCallBackDelegate VideoPlayEventCallBackEvent;
+
+        public bool VideoClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VideoPlay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
