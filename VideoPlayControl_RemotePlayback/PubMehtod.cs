48baa3f28e43579b9accf7ac7b8699f784d26aef
thankyou1996 <1296214557@qq.com>
2019-08-09 16:02:19 +0800

    1.添加获取远程录像文件信息接口

---
+++ VideoPlayControl_RemotePlayback/PubMehtod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VideoCurrencyModule.RemotePlayback;
+
+namespace VideoPlayControl_RemotePlayback
+{
+    public class PubMehtod
+    {
+
+        /// <summary>
+        /// 获取远程回访文件信息
+        /// </summary>
+        /// <param name="strPath"></param>
+        /// <returns></returns>
+        public static List<RemoteBackplayFileInfo> GetRemoteBackplayFileInfo_SKN(string strPath)
+        {
+            List<RemoteBackplayFileInfo> result = new List<RemoteBackplayFileInfo>();
+
+            return result;
+        }
+    }
+}
