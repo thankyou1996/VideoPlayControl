fdb9313035c5a57103631a9afa42d5be118e032b
thankyou1996 <1296214557@qq.com>
2019-08-14 15:56:43 +0800

    VideoPlayControlRemotePlayer.PubMehtod 完善

--- SKVideoRemotePlayer/PubMethod.cs
+++ SKVideoRemotePlayer/PubMethod.cs
@@ -20,9 +20,18 @@ namespace SKVideoRemotePlayer
             VideoInfo vInfo = cInfo.VideoInfo;
             string strPath1 = SDK_SKNVideo.GetFileMapPath(cInfo);
             string strPath2 = SDK_SKNVideo.GetLocalFileMapPath(cInfo);
-            string Temp_strPath = Path.GetDirectoryName(strPath2);
+            string Temp_strPath = ProgPara.CurrentProgPara.DefaultSaveDir + Path.GetDirectoryName(strPath2);
+            CommonMethod.Common.CreateFolder(Temp_strPath);
             SDK_SKNVideo.SDK_NSK_CLIENT_get_file(vInfo.DVSAddress, false, strPath1, strPath2);
             CommonMethod.Common.Delay_Millisecond(1500);
+        }
+
+        public static string GetFileMapPath(CameraInfo cInfo)
+        {
+            StringBuilder sbResult = new StringBuilder();
+            sbResult.Append(ProgPara.CurrentProgPara.DefaultSaveDir);
+            sbResult.Append(SDK_SKNVideo.GetLocalFileMapPath(cInfo));
+            return sbResult.ToString();
         }
     }
 }
